9b23112666b9f6e825b2e556b707f72744c09afe
Thomas Pinaud <pinaudt@gmail.com>
2013-12-18 20:58:07 +0100

    Retrieved pictures date

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var cheerio = require('cheerio');
 var fs      = require('fs');
 
 
-var url = 'http://mars.jpl.nasa.gov/msl/multimedia/raw/?s=93&camera=FHAZ_'; //12
+var url = 'http://mars.jpl.nasa.gov/msl/multimedia/raw/?s=437&camera=FHAZ_'; //12
 //var url = 'http://mars.jpl.nasa.gov/msl/multimedia/raw/?s=21&camera=FHAZ_'; //2
 
 request({ 'uri' : url }, function (err, response, body){
@@ -13,23 +13,27 @@ request({ 'uri' : url }, function (err, response, body){
         return;
     }
 
-    var answer = parseD(body);
+    var answer = parse(body);
 });
 
 
 var parse = function(html){
-    var alt = 'Image taken by Front Hazcam: Right A';
+    var alt = 'Image taken by Front Hazcam: Right B';
     var $ = cheerio.load(html);
 
-    console.log("DL ok");
-    var list = $('body').find('img[alt="'+alt+'"]').toArray();
+    console.log("Page load.");
+    var list = $('body').find('img[alt="'+alt+'"][width="160"]').toArray();
     //
     for(var i in list){
         if(list[i].attribs && list[i].attribs.src){
             //console.log($(list[i]).parent());
             var src = (list[i].attribs.src).replace('-thm.jpg', '.JPG');
-            console.log(src);
-            saveImg(src, i+".jpg");
+            var el = $(list[i]).parent().next().next().find('.RawImageUTC');
+            el.children().remove();
+            var picDate =  el.text();
+            picDate = picDate.replace(' UTC', '');
+            console.log(picDate);
+            saveImg(src, picDate+".jpg");
         }
     }
 };
@@ -37,7 +41,7 @@ var parse = function(html){
 
 var parseD = function(html){
     var $ = cheerio.load(html);
-    var content = "Front Hazcam: Right A";
+    var content = "Front Hazcam: Right B";
 
     console.log("DL ok");
     var list = $('body').find("table:contains('FULL')  div.RawImageCaption:contains('"+content+"')").toArray();
@@ -53,8 +57,8 @@ var parseD = function(html){
 var saveImg = function(url, filename){
 
     request.head(url, function(err, res, body){
-        console.log('content-type:', res.headers['content-type']);
-        console.log('content-length:', res.headers['content-length']);
+        //console.log('content-type:', res.headers['content-type']);
+        //console.log('content-length:', res.headers['content-length']);
 
         request(url).pipe(fs.createWriteStream("export/"+filename));
     });
