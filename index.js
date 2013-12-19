8f495280de061d694cbf76860b3cf6a6879a96f7
Thomas Pinaud <pinaudt@gmail.com>
2013-12-19 16:46:04 +0100

    Cleaning

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ var cheerio = require('cheerio');
 var fs      = require('fs');
 var Q       = require('q');
 
-var position = "Left";
-var camera   = "A";
 
 var urls = new Array();
 var index = 0;
+
+
+var position = "Right";
+var camera   = "B";
+
+
 var loadAdresses = function(){
     var url = 'http://mars.jpl.nasa.gov/msl/multimedia/raw/';
     request({ 'uri' : url }, function (err, response, body){
@@ -19,13 +23,11 @@ var loadAdresses = function(){
 
         var $ = cheerio.load(body);
         var list = $('body').find('div.image_title:contains("Front Hazard Avoidance Cameras (Front Hazcams)")').next().children('.image_list').children('ul').children('li').children('a').toArray();
-        //console.log(list);
+
         for(var i in list){
             var url = list[i].attribs.href;
             url = url.replace('./', 'http://mars.jpl.nasa.gov/msl/multimedia/raw/');
-            //if(i < 5){
-                urls.push(url);
-            //}
+            urls.push(url);
         }
         nextPage();
     });
@@ -73,7 +75,7 @@ var parse = function(html){
     //
     for(var i in list){
         if(list[i].attribs && list[i].attribs.src){
-            //console.log($(list[i]).parent());
+            //
             var src = (list[i].attribs.src).replace('-thm.jpg', '.JPG');
             var el = $(list[i]).parent().next().next().find('.RawImageUTC');
             el.children().remove();
@@ -103,8 +105,6 @@ var saveImg = function(url, filename){
     var defer = Q.defer();
 
     request.head(url, function(err, res, body){
-        //console.log('content-type:', res.headers['content-type']);
-        //console.log('content-length:', res.headers['content-length']);
 
         var writer = fs.createWriteStream("exports/"+position+camera+"/"+filename);
 
@@ -118,43 +118,3 @@ var saveImg = function(url, filename){
 
     return defer.promise;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-var parseD = function(html){
-    var $ = cheerio.load(html);
-    var content = "Front Hazcam: Right B";
-
-    console.log("DL ok");
-    var list = $('body').find("table:contains('FULL')  div.RawImageCaption:contains('"+content+"')").toArray();
-    //
-    for(var i in list){
-        var href = $(list[i]).find("a")[0].attribs.href;
-        saveImg(href, i+".jpg");
-    }
-
-}
-*/
