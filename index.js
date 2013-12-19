41ac011fe4afce987da6aa79b57bd23f574def0e
Thomas Pinaud <pinaudt@gmail.com>
2013-12-19 16:42:43 +0100

    Update gitignore and export foldres

--- index.js
+++ index.js
@@ -1,28 +1,75 @@
 var request = require('request');
 var cheerio = require('cheerio');
 var fs      = require('fs');
+var Q       = require('q');
+
+var position = "Left";
+var camera   = "A";
+
+var urls = new Array();
+var index = 0;
+var loadAdresses = function(){
+    var url = 'http://mars.jpl.nasa.gov/msl/multimedia/raw/';
+    request({ 'uri' : url }, function (err, response, body){
+
+        if (err || response.statusCode != 200) {
+            console.log('Bad response');
+            return;
+        }
+
+        var $ = cheerio.load(body);
+        var list = $('body').find('div.image_title:contains("Front Hazard Avoidance Cameras (Front Hazcams)")').next().children('.image_list').children('ul').children('li').children('a').toArray();
+        //console.log(list);
+        for(var i in list){
+            var url = list[i].attribs.href;
+            url = url.replace('./', 'http://mars.jpl.nasa.gov/msl/multimedia/raw/');
+            //if(i < 5){
+                urls.push(url);
+            //}
+        }
+        nextPage();
+    });
+}
+
+loadAdresses();
 
 
-var url = 'http://mars.jpl.nasa.gov/msl/multimedia/raw/?s=437&camera=FHAZ_'; //12
-//var url = 'http://mars.jpl.nasa.gov/msl/multimedia/raw/?s=21&camera=FHAZ_'; //2
-
-request({ 'uri' : url }, function (err, response, body){
-
-    if (err || response.statusCode != 200) {
-        console.log('Bad response');
-        return;
+var nextPage = function(){
+    if(index < urls.length){
+        loadPage(urls[index]);
     }
+    index++;
+}
 
-    var answer = parse(body);
-});
 
+
+var loadPage = function(url){
+    //var url = 'http://mars.jpl.nasa.gov/msl/multimedia/raw/?s=21&camera=FHAZ_'; //2
+
+    request({ 'uri' : url }, function (err, response, body){
+
+        if (err || response.statusCode != 200) {
+            console.log('Bad response');
+            return;
+        }
+
+        console.log("Page loaded. - "+url);
+        var answer = parse(body);
+    });
+}
 
 var parse = function(html){
-    var alt = 'Image taken by Front Hazcam: Right B';
+    var alt = 'Image taken by Front Hazcam: '+position+' '+camera;
     var $ = cheerio.load(html);
 
-    console.log("Page load.");
+    
     var list = $('body').find('img[alt="'+alt+'"][width="160"]').toArray();
+    var loading = 0;
+    if(list.length == 0){
+        nextPage();
+
+        return;
+    }
     //
     for(var i in list){
         if(list[i].attribs && list[i].attribs.src){
@@ -32,13 +79,71 @@ var parse = function(html){
             el.children().remove();
             var picDate =  el.text();
             picDate = picDate.replace(' UTC', '');
+            picDate = picDate.replace(/\s/g, '-');
+            picDate = picDate.replace(/:/g, '-');
             console.log(picDate);
-            saveImg(src, picDate+".jpg");
+            loading++;
+            saveImg(src, picDate+".jpg").then(function(){
+
+                loading--;
+                if(loading == 0){
+                    console.log('Pics loaded');
+                    nextPage();
+                }
+            });
         }
     }
 };
 
 
+
+
+var saveImg = function(url, filename){
+
+    var defer = Q.defer();
+
+    request.head(url, function(err, res, body){
+        //console.log('content-type:', res.headers['content-type']);
+        //console.log('content-length:', res.headers['content-length']);
+
+        var writer = fs.createWriteStream("exports/"+position+camera+"/"+filename);
+
+        var dl = request(url)
+            .on('end', function(){
+                defer.resolve(true);
+            })
+            .pipe(writer)
+        ;
+    });
+
+    return defer.promise;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 var parseD = function(html){
     var $ = cheerio.load(html);
     var content = "Front Hazcam: Right B";
@@ -52,14 +157,4 @@ var parseD = function(html){
     }
 
 }
-
-
-var saveImg = function(url, filename){
-
-    request.head(url, function(err, res, body){
-        //console.log('content-type:', res.headers['content-type']);
-        //console.log('content-length:', res.headers['content-length']);
-
-        request(url).pipe(fs.createWriteStream("export/"+filename));
-    });
-}
+*/
