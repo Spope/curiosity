28b287aae1a2574347dbccec8339f7fd41da2a35
Thomas Pinaud <pinaudt@gmail.com>
2014-01-09 16:51:53 +0100

    Commented new ffmpeg commands. Need to compile ffmpeg with libx264...

--- modules/video.js
+++ modules/video.js
@@ -18,6 +18,7 @@ module.exports = {
          * -qscale 1 > max quality
          */
         exec("ffmpeg -r 10 -y -qscale 1 -i "+sources+" "+destMP4, function(error, stdout, stderr){
+        //exec("~/bin/ffmpeg -r 10 -y -i "+sources+" -vcodec libx264 -preset slow -crf 24 -pix_fmt yuv420p "+destMP4+"", function(error, stdout, stderr){
             console.log('MP4 video encoded'.green);
 
             if (error !== null) {
@@ -25,6 +26,7 @@ module.exports = {
             }
 
             exec("ffmpeg -r 10 -y -qscale 15 -i "+sources+" "+destOGG, function(error, stdout, stderr){
+            //exec("~/bin/ffmpeg -r 10 -y -i "+sources+" "+destOGG, function(error, stdout, stderr){
                 console.log('OGG video encoded'.green);
 
                 if (error !== null) {
