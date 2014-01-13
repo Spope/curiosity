9db48bf5a8a6c6ac752537a2e2e460933ed16bea
Thomas Pinaud <pinaudt@gmail.com>
2014-01-13 10:50:32 +0100

    Fixed encoding

--- modules/video.js
+++ modules/video.js
@@ -15,9 +15,9 @@ module.exports = {
          * -i input name
          * -r framerate
          * -y overwrite
-         * -qscale 1 > max quality
+         * 
          */
-        //exec("ffmpeg -r 10 -y -qscale 1 -i "+sources+" "+destMP4, function(error, stdout, stderr){
+        //compiled version
         exec("~/bin/ffmpeg -r 10 -y -i "+sources+" -vcodec libx264 -preset slow -crf 24 -pix_fmt yuv420p "+destMP4+"", function(error, stdout, stderr){
             console.log('MP4 video encoded'.green);
 
@@ -25,8 +25,8 @@ module.exports = {
                 console.log('exec error: '.red + error);
             }
 
-            //exec("ffmpeg -r 10 -y -qscale 15 -i "+sources+" "+destOGG, function(error, stdout, stderr){
-            exec("~/bin/ffmpeg -r 10 -y -i "+sources+" "+destOGG, function(error, stdout, stderr){
+            //ubuntu version
+            exec("/usr/bin/ffmpeg -r 10 -y -i "+sources+" -qscale 15 "+destOGG, function(error, stdout, stderr){
                 console.log('OGG video encoded'.green);
 
                 if (error !== null) {
@@ -36,5 +36,10 @@ module.exports = {
         });
     }
 };
+//ubuntu version (installed wit apt-get)
 //  /usr/bin/ffmpeg -r 10 -y  -i ./exports/merge/%05d.jpg -qscale 1 ./exports/video.mp4
 //  /usr/bin/ffmpeg -r 10 -y  -i ./exports/merge/%05d.jpg -qscale 15 ./exports/video.ogg
+
+//compiled version (https://trac.ffmpeg.org/wiki/UbuntuCompilationGuide)
+// ~/bin/ffmpeg -r 10 -y -i ./exports/merge/%05d.jpg ./test.ogv
+//~/bin/ffmpeg -r 10 -y -i ./exports/merge/%05d.jpg -vcodec libx264 -preset slow -crf 24 -pix_fmt yuv420p ./test.mp4
