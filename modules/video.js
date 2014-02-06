39705c283bca0ae085f8c96c5a5548fb80622af1
Thomas Pinaud <pinaudt@gmail.com>
2014-02-06 15:06:59 +0100

    Webm bitrate

--- modules/video.js
+++ modules/video.js
@@ -16,16 +16,15 @@ module.exports = {
         var destOGG_big = './exports/video_big.webm';
 
         console.log('start video encoding'.cyan);
-        /*
+
         that.doMP4(sources, destMP4).then(function(){
             return that.doOGG(sources, destOGG, "1M");
         }).then(function(){
             return that.doMP4(sources_big, destMP4_big);
         }).then(function(){
-            return that.doOGG(sources_big, destOGG_big, "10M");
+            return that.doOGG(sources_big, destOGG_big, "5M");
         }).done();
-       */
-        that.doOGG(sources_big, destOGG_big, "5M");
+
     },
 
     doMP4: function(sources, dest){
