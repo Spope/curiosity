fd4b0046556c76f25251caa29d0468e0a62b6504
Thomas Pinaud <pinaudt@gmail.com>
2014-04-22 19:34:34 +0200

    anticache css

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <title>Curiosity's Trip</title>
     <!-- <meta name="viewport" content="width=device-width, user-scalable=no"> -->
-    <link rel="stylesheet" type="text/css" href="public/css/main.css" media="all" />
+    <link rel="stylesheet" type="text/css" href="public/css/main.css?cache=78" media="all" />
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css' />
     <link rel="image_src" href="http://projects.spope.fr/curiosity/exports/merge/00469.jpg" />
     <meta property="og:image" content="http://projects.spope.fr/curiosity/exports/merge/00469.jpg"/>
